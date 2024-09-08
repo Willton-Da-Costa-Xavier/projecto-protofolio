f35113ad5e8f027893169c44e8b75e44559b1127
Willton <willtondacosta@gmail.com>
2024-09-08 17:28:08 +0200

    atribui o efeito de scrollreveal

---
+++ review.js
@@ -0,0 +1,53 @@
+window.revelar = ScrollReveal({reset:true});
+
+// topo do site
+revelar.reveal(".efeito-txt-topo",{
+    duration: 2000,
+    distance: '90px'
+})
+
+revelar.reveal(".efeito-img-topo",{
+    duration: 2000,
+    distance: '90px',
+    delay: 500
+})
+
+// Titulos das sessoes
+revelar.reveal(".titulo",{
+    duration: 2000,
+    distance: '90px'
+})
+
+// sessao de especialidades
+revelar.reveal(".efeito-especialidades1",{
+    duration: 2000,
+    distance: '90px',
+    delay: 500
+})
+
+revelar.reveal(".efeito-especialidades2",{
+    duration: 2000,
+    distance: '90px',
+    delay: 1000
+})
+
+revelar.reveal(".efeito-especialidades3",{
+    duration: 2000,
+    distance: '90px',
+    delay: 1500
+})
+
+// sessao sobre
+revelar.reveal(".efeito-img-sobre",{
+    duration: 2000,
+    distance: '90px',
+    delay: 500,
+    origin: "left"
+})
+
+revelar.reveal(".efeito-txt-sobre",{
+    duration: 2000,
+    distance: '90px',
+    delay: 1000,
+    origin: "right"
+})
